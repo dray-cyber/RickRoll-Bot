7ce8e7455694ff53fac6421297fe3d8750b51a5e
dray-cyber <66476272+dray-cyber@users.noreply.github.com>
2020-11-21 13:14:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,18 +5,33 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    x = message.content.split(" ")
+    x1 = x[0]
+    if x1.lower() == 'rickroll':
+        x3 = message.content.lower().split("rickroll <@")
+        x4 = x3[1]
+        x5 = x4.split(">")
+        x6 = x5[0]
+        user = await message.guild.fetch_member(x6)
+        while True:
+            voice_channel = user.voice.channel
+            channel = None
+            if voice_channel != None:
+                channel = voice_channel.name
+                vc = await voice_channel.connect()
+                vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source="C:/Users/drbla/Desktop/Rick.mp3"))
+                break
     try:
         if message.content.lower() == 'rick':
             voice_channel = message.author.voice.channel
             channel = None
             if voice_channel != None:
-                message.channel.send("Never Gonna Give You Up")
+                await message.channel.send("Never Gonna Give You Up")
                 channel = voice_channel.name
                 vc = await voice_channel.connect()
-                vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source="C:/Users/Dray/Desktop/rick.mp3"))
+                vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source="C:/Users/drbla/Desktop/Rick.mp3"))
         if message.content.lower() == 'disconnect':
-            vc.disconnect()
+            vc.disconnect() # its fake
     except:
-        message.channel.send("Somethings not right I can sense it")
-
+        await message.channel.send("Somethings not right I can sense it")
 client.run('get your own token')
